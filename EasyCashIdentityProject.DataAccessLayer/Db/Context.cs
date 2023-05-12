8c11057345a3ad5b770ecd82e262aaf8ea0f7932
Ruslan Valibayli <valibayli91@gmail.com>
2023-05-12 22:29:05 +0400

    added references between layers, created context class in DAL

---
+++ EasyCashIdentityProject.DataAccessLayer/Db/Context.cs
@@ -0,0 +1,21 @@
+﻿using EasyCashIdentityProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.DataAccessLayer.Db
+{
+    public class Context : IdentityDbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server = DESKTOP-UJ12B0H\\SQLEXPRESS; initial catalog = EasyCashDb; integrated security = true;");
+        }
+        public DbSet<CustomerAccount> CustomerAccounts { get; set; }
+        public DbSet<CustomerAccountProcess> CustomerAccountProcesses { get; set; }
+    }
+}
