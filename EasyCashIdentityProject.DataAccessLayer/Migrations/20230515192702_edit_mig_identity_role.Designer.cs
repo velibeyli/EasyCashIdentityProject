8ef5daa402fac6b00e9d199df933e00d9477fde8
Ruslan Valibayli <valibayli91@gmail.com>
2023-05-15 23:57:27 +0400

    appRole and appUser classes added

--- EasyCashIdentityProject.DataAccessLayer/Migrations/20230515192702_edit_mig_identity_role.Designer.cs
+++ EasyCashIdentityProject.DataAccessLayer/Migrations/20230515192702_edit_mig_identity_role.Designer.cs
@@ -4,6 +4,7 @@ using EasyCashIdentityProject.DataAccessLayer.Db;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EasyCashIdentityProject.DataAccessLayer.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20230515192702_edit_mig_identity_role")]
+    partial class edit_mig_identity_role
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
