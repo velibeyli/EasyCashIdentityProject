7d09ea8cd14ecc891d7dc7f72a5183c173c999d6
Ruslan Valibayli <valibayli91@gmail.com>
2023-05-16 23:46:22 +0400

    repository interface and implementations added

--- EasyCashIdentityProject.BusinessLayer/ValidationRules/AppUserValidationRules/AppUserRegisterValidator.cs
+++ EasyCashIdentityProject.BusinessLayer/ValidationRules/AppUserValidationRules/AppUserRegisterValidator.cs
@@ -1,4 +1,5 @@
-﻿using FluentValidation;
+﻿using EasyCashIdentityProject.DtoLayer.AppUserDtos;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,30 @@ namespace EasyCashIdentityProject.BusinessLayer.ValidationRules.AppUserValidatio
 {
     public class AppUserRegisterValidator : AbstractValidator<AppUserRegisterDto>
     {
+        public AppUserRegisterValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Name field can not be empty!")
+                .NotNull().WithMessage("Name field can not be null!")
+                .MaximumLength(30).WithMessage("Name can not be more than 30 character!")
+                .MinimumLength(3).WithMessage("Name should be at least 3 character");
+            RuleFor(x => x.Surname)
+                .NotEmpty().WithMessage("Surname field can not be empty!")
+                .NotNull().WithMessage("Surname field can not be null!");
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("Email field can not be empty!")
+                .NotNull().WithMessage("Email field can not be null!")
+                .EmailAddress().WithMessage("Please enter true email address!");
+            RuleFor(x => x.Username)
+                .NotEmpty().WithMessage("Username field can not be empty!")
+                .NotNull().WithMessage("Username field can not be null!");
+            RuleFor(x => x.Password)
+                .NotEmpty().WithMessage("Password field can not be empty!")
+                .NotNull().WithMessage("Password field can not be null!");
+            RuleFor(x => x.ConfirmPassword)
+                .NotEmpty().WithMessage("Confirm password field can not be empty!")
+                .NotNull().WithMessage("Confirm password field can not be null!")
+                .Equal(y => y.Password).WithMessage("password and confirm password does not matches!");
+        }
     }
 }
