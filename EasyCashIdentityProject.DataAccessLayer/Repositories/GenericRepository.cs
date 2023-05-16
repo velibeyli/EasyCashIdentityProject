75b936b6cdccc58706a1ceeb653a9661d43acda8
Ruslan Valibayli <valibayli91@gmail.com>
2023-05-16 23:47:12 +0400

    interfaces and implementations of repository added

---
+++ EasyCashIdentityProject.DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,46 @@
+﻿using EasyCashIdentityProject.DataAccessLayer.Db;
+using EasyCashIdentityProject.DataAccessLayer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.DataAccessLayer.Repositories
+{
+    public class GenericRepository<T> : IGenericDAL<T> where T : class
+    {
+        public void Delete(T entity)
+        {
+            using var context = new Context();
+            context.Set<T>().Remove(entity);
+            context.SaveChanges();
+        }
+
+        public List<T> GetAll()
+        {
+            using var context = new Context();
+            return context.Set<T>().ToList();
+        }
+
+        public T GetById(int id)
+        {
+            using var context = new Context();
+            return context.Set<T>().Find(id);
+        }
+
+        public void Insert(T entity)
+        {
+            using var context = new Context();
+            context.Set<T>().Add(entity);
+            context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            using var context = new Context();
+            context.Set<T>().Update(entity);
+            context.SaveChanges();
+        }
+    }
+}
