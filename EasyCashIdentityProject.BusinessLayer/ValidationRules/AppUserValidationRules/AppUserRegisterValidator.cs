4d329692b8c956fdae930beb0d671e3a215f7df3
Ruslan Valibayli <valibayli91@gmail.com>
2023-05-16 23:00:46 +0400

    fluent validation added to the project

---
+++ EasyCashIdentityProject.BusinessLayer/ValidationRules/AppUserValidationRules/AppUserRegisterValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.BusinessLayer.ValidationRules.AppUserValidationRules
+{
+    public class AppUserRegisterValidator : AbstractValidator<AppUserRegisterDto>
+    {
+    }
+}
