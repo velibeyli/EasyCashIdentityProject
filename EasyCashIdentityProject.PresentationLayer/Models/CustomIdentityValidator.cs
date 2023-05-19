207e13ecf4c600610f6d7af10a01db0b070f39bc
Ruslan Valibayli <valibayli91@gmail.com>
2023-05-19 23:36:08 +0400

    customIdentityValidator class created and inject to program.cs (translate errors to Azerbaijani)

---
+++ EasyCashIdentityProject.PresentationLayer/Models/CustomIdentityValidator.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace EasyCashIdentityProject.PresentationLayer.Models
+{
+	public class CustomIdentityValidator : IdentityErrorDescriber
+	{
+		public override IdentityError PasswordTooShort(int length)
+		{
+			return new IdentityError()
+			{
+				Code = "PasswordTooShort",
+				Description = $"Şifrə ən azı {length} character olmalıdır!"
+			};
+		}
+
+		public override IdentityError PasswordRequiresUpper()
+		{
+			return new IdentityError()
+			{
+				Code = "PasswordRequiresUpper",
+				Description = "Password ən azı 1 böyük hərfdən ibarət olmalıdır!"
+			};
+		}
+
+		public override IdentityError PasswordRequiresLower()
+		{
+			return new IdentityError()
+			{
+				Code = "PasswordRequiresLower",
+				Description = "Password ən azı 1 kiçik hərfdən ibarət olmalıdır!"
+			};
+		}
+
+		public override IdentityError PasswordRequiresNonAlphanumeric()
+		{
+			return new IdentityError()
+			{
+				Code = "PasswordRequiresNonAlphanumeric",
+				Description = "Password ən azı bir simvoldan ibarət olmalıdır!"
+			};
+		}
+
+		public override IdentityError PasswordRequiresDigit()
+		{
+			return new IdentityError()
+			{
+				Code = "PasswordRequiresDigit",
+				Description = "Password ən azı bir rəqəmdən ibarət olmalıdır!"
+			};
+		}
+	}
+}
